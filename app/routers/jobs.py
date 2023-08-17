698a61223b6febd1be94091e5f292c1dde5bd097
emadbastawisi <emadbastawisi@outlook.com>
2023-08-17 23:46:14 +0300

    updated add-search-keyword logic

--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -16,10 +16,11 @@ router = APIRouter(
 
 # all new jobs to the database every 24 hours
 @router.get('/', status_code=status.HTTP_201_CREATED)
-def scrap_jobs( db: Session = Depends(get_db), current_user: int = Depends(Oauth2.get_current_user)):
+def scrap_jobs(db: Session = Depends(get_db), current_user: int = Depends(Oauth2.get_current_user)):
     try:
         # Call the function get_jobs_from_wuzzuf_toDb with the provided URL.
-        get_jobs_from_wuzzuf_toDb('https://wuzzuf.net/search/jobs/?filters%5Bpost_date%5D%5B0%5D=within_24_hours&start=0' , db)
+        get_jobs_from_wuzzuf_toDb(
+            'https://wuzzuf.net/search/jobs/?filters%5Bpost_date%5D%5B0%5D=within_24_hours&start=0', db)
 
         # Return the HTTP status code 201 Created indicating that the request was successful.
         return status.HTTP_201_CREATED
@@ -31,13 +32,14 @@ def scrap_jobs( db: Session = Depends(get_db), current_user: int = Depends(Oauth
 
 # search jobs based on user list of selected search keywords
 @router.get('/{request}', response_model=list[schemas.JobOut])
-def get_jobs(request: str , db: Session = Depends(get_db), current_user: int = Depends(Oauth2.get_current_user)):
+def get_jobs(request: str, db: Session = Depends(get_db), current_user: int = Depends(Oauth2.get_current_user)):
     # Split the search keywords by commas into a list
     list_of_keywords = request.split(',')
     # Perform a database query to find jobs that have any of the keywords in their skills or title
     keyword_results = db.query(models.Job).filter(or_(*[models.Job.skills.ilike(f'%{keyword}%') for keyword in list_of_keywords], *[
-        models.Job.title.ilike(f'%{keyword}%') for keyword in list_of_keywords])).all()
+        models.Job.title.ilike(f'%{keyword}%') for keyword in list_of_keywords]))
+    print(keyword_results)
+    keyword_results = keyword_results.all()
 
     # Return the keyword results
     return keyword_results
-
